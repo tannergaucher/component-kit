50afe8a618329898d679b7589a739196383c06a9
tanner gaucher <tannermichaelgaucher@gmail.com>
2018-08-29 13:09:30 +0700

    Init

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Library, Example } from '@compositor/kit'
+import { Button } from 'standard-components'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './theme'
+
+export default () => (
+  <ThemeProvider theme={theme}>
+    <Library>
+      <Library.Nav />
+      <Example name="Button">
+        <Button bg="gray9">Hello, world!</Button>
+      </Example>
+    </Library>
+  </ThemeProvider>
+)
